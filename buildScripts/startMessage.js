126d8a5d6f0e18c9ad75eacff77113230158740c
Jose Coca <jr@mcg.dk>
2021-12-13 20:40:17 -0400

    configure babel and use import syntax in scripts

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 // this is a library for adding coloured messages to the output console
-var chalk = require("chalk");
+import chalk from "chalk";
 
 console.log(chalk.green("Starting app in dev mode..."));
