84be18b38171b3ac836a0776906e4376d9f8bba1
Jose Coca <jr@mcg.dk>
2021-12-16 23:25:06 -0400

    html test with cheerio

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import jsdom from "jsdom";
 import fs from "fs";
+import * as cheerio from 'cheerio';
 
 
 describe("Our first test", () => {
@@ -17,5 +18,10 @@ describe("index.html", () => {
         const h1 = dom.window.document.getElementsByTagName("h1")[0];
         expect(h1.innerHTML).to.equal("Hello World!!!");
         dom.window.close();
+    });
+    it("should say hello with cheerio", () => {
+        const index = fs.readFileSync("./src/index.html", "utf-8");
+        let $ = cheerio.load(index);
+        expect($('h1').text()).to.equal("Hello World!!!");
     });
 });
