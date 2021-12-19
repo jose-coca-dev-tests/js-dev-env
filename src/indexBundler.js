ed433251375847ea7857cb0d2ec2099d1d074655
Jose Coca <jr@mcg.dk>
2021-12-19 01:04:49 -0400

    consume users api and display in bundle.html

--- src/indexBundler.js
+++ src/indexBundler.js
@@ -1,3 +1,22 @@
 import "./styles.css";
 import {LibConst} from './myLib.js';
 console.log("indexBundler.js is loaded!!, from lib: " + LibConst.toString()); // eslint-disable-line no-console
+
+import { getUsers } from "./api/userApi";
+
+// Populate table of users via API call.
+getUsers().then((result) => {
+  let usersBody = "";
+
+  result.forEach((user) => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`;
+  });
+
+  global.document.getElementById("users").innerHTML = usersBody;
+});
