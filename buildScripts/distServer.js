88b50e009201bcf12ce57f6e978d8ec1f3e6102f
Jose Coca <jr@mcg.dk>
2021-12-20 01:36:00 -0400

    updates for running production in local

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,40 +1,19 @@
 import express from "express";
 import path from "path";
 import open from "open";
-import webpack from "webpack";
-import config from "../webpack.config.dev";
+import compression from "compression";
 
 // next line will disable the console rule of eslint, it is proper to do so in build scripts like this
 /* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
-app.use(
-  require("webpack-dev-middleware")(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
+app.use(express.static("dist"));
+app.use(compression());
 
 app.get("/bundle.html", function (req, res) {
-  res.sendFile(path.join(__dirname, "../src/bundle.html"));
-});
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../src/index.html"));
-});
-
-app.get('/index.js',function(req,res){
-    res.sendFile(path.join(__dirname, '../src/index.js')); 
-});
-
-app.get('/myLib.js',function(req,res){
-    res.sendFile(path.join(__dirname, '../src/myLib.js')); 
-});
-
-app.get('/styles.css',function(req,res){
-    res.sendFile(path.join(__dirname, '../src/styles.css')); 
+  res.sendFile(path.join(__dirname, "../dist/bundle.html"));
 });
 
 app.get("/users", function(req, res) {
