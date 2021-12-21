bd1613ce6ed56387b5ec6556767eedc3d9da6005
Jose Coca <jr@mcg.dk>
2021-12-21 00:54:47 -0400

    bundle splitting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,11 +4,14 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 export default {
   mode: "production",
   devtool: "source-map",
-  entry: "./src/indexBundler.js",
+  entry: {
+    main: path.resolve(__dirname, "src/indexBundler"),
+    vendor: path.resolve(__dirname, "src/vendor"),
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "bundle.js",
+    filename: "[name].js",
   },
   plugins: [
     // Create HTML file that includes reference to bundled JS.
