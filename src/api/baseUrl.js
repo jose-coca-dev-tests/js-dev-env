b2a20311e6b29c0693fd39b59d7f9716d116dd25
Jose Coca <jr@mcg.dk>
2021-12-20 23:36:47 -0400

    consume production or mock api by parameter

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,17 @@
 export default function getBaseUrl() {
-  const inDevelopment = window.location.hostname.toLowerCase() === "localhost";
-  return inDevelopment ? "http://localhost:3001/" : "/";
+  let productionApiURL = '/'; // this should be replaced by your production API url
+  let prm = getQueryStringParameterByName('useMockApi');
+  if(prm)
+    prm = prm.toLowerCase();
+  return prm === "true" ? 'http://localhost:3001/' : productionApiURL;
+}
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
